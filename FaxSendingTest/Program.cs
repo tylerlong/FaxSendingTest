bc0bbe073507109b68beed7a2eb89003d9127c60
Tyler Long <tyler4long@gmail.com>
2020-07-30 14:00:25 -0700

    hello world

--- FaxSendingTest/Program.cs
+++ FaxSendingTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
+using dotenv.net;
+using RingCentral;
 
 namespace FaxSendingTest
 {
@@ -6,7 +9,26 @@ namespace FaxSendingTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DotEnv.Config(true);
+            
+            Task.Run(async () =>
+            {
+                Console.WriteLine("Hello world!");
+
+                // using (var rc = new RestClient(
+                //     Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                //     Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                //     Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+                // ))
+                // {
+                //     await rc.Authorize(
+                //         Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                //         Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                //         Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                //     );
+                // }
+
+            }).GetAwaiter().GetResult();
         }
     }
 }
