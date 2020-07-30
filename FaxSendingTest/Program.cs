c4adae5d670caa59ea055cc1d8584c4d9bd83c5e
Tyler Long <tyler4long@gmail.com>
2020-07-30 14:11:25 -0700

    Send a fax

--- FaxSendingTest/Program.cs
+++ FaxSendingTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using dotenv.net;
 using RingCentral;
@@ -13,20 +14,33 @@ namespace FaxSendingTest
             
             Task.Run(async () =>
             {
-                Console.WriteLine("Hello world!");
+                using (var rc = new RestClient(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+                ))
+                {
+                    await rc.Authorize(
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                    );
 
-                // using (var rc = new RestClient(
-                //     Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
-                //     Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
-                //     Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
-                // ))
-                // {
-                //     await rc.Authorize(
-                //         Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
-                //         Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
-                //         Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
-                //     );
-                // }
+                    await rc.Restapi().Account().Extension().Fax().Post(new CreateFaxMessageRequest
+                    {
+                        attachments = new []{ new Attachment
+                        {
+                            fileName = "TestingFile.pdf",
+                            contentType = "application/pdf", 
+                            bytes = File.ReadAllBytes(Environment.GetEnvironmentVariable("PDF_FILE_TO_SEND"))
+                        }},
+                        to = new []{ new MessageStoreCalleeInfoRequest
+                        {
+                            phoneNumber = Environment.GetEnvironmentVariable("RINGCENTRAL_RECEIVER")
+                        } }
+                    });
+                    Console.WriteLine("Fax sent");
+                }
 
             }).GetAwaiter().GetResult();
         }
